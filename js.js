d13e95e364cf4e43f06170c906e2b00f46432690
merahn <merahn@iu.edu>
2025-04-14 19:36:28 -0400

    fixing stuff

--- js.js
+++ js.js
@@ -100,24 +100,33 @@ document.addEventListener("DOMContentLoaded", function () {
   var modal = document.getElementById("imageModal");
   var modalImage = document.getElementById("modalImage");
   var closeButton = document.getElementsByClassName("close")[0];
-  var images = document.getElementsByClassName("clickable-image");
 
-  if (modal && modalImage && closeButton) {
-    for (var i = 0; i < images.length; i++) {
-      images[i].onclick = function () {
+  function attachClickEvents() {
+    var images = document.querySelectorAll(".clickable-image img"); // make sure we're targeting the <img> not the div
+    images.forEach(img => {
+      img.onclick = function () {
         modal.style.display = "flex";
         modalImage.src = this.src;
+        document.getElementById("caption").innerText = this.alt || "";
       };
-    }
-
-    closeButton.onclick = function () {
-      modal.style.display = "none";
-    };
-
-    window.onclick = function (event) {
-      if (event.target == modal) {
-        modal.style.display = "none";
-      }
-    };
+    });
   }
+
+  // Attach events initially
+  attachClickEvents();
+
+  // Also attach on toggle of <details> to catch newly shown content
+  document.querySelectorAll("details").forEach(detail => {
+    detail.addEventListener("toggle", attachClickEvents);
+  });
+
+  closeButton.onclick = function () {
+    modal.style.display = "none";
+  };
+
+  window.onclick = function (event) {
+    if (event.target === modal) {
+      modal.style.display = "none";
+    }
+  };
 });
